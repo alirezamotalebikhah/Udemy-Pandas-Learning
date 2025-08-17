de5bc992b2af4e3de05309d7fad63c3d9f048a3c
alirezamotalebikhah <alirezamotalebikhah@gmail.com>
2025-08-17 15:20:38 +0330

    initial files

---
+++ Video 10/homework of video 6.py
@@ -0,0 +1,16 @@
+years = int(input("Enter the number of years: "))
+
+if 0<=years<=2:
+    print("You are an infant baby")
+elif 3<=years<=12:
+    print("You are child")
+elif 13<=years<=19:
+    print("You are a teen")
+elif 20<=years<=35:
+    print("You are a young star")
+elif 36<=years<=50:
+    print("You are matured man and you should have wife and kids")
+else:
+    print("You are national treasure and we love you")
+
+
